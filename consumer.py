11e6373694a6db4e5b89f95dda8eaca5e6f431cb
arashkhavari <khavari@riseup.net>
2020-02-19 00:03:15 +0330

    Refactor consumer.py

--- consumer.py
+++ consumer.py
@@ -1,32 +1,27 @@
 #!/bin/python3
 
-from kafka import KafkaConsumer, TopicPartition
 import json
+import logging
+import os
+from pathlib import Path
 import update_extractor
 import insert_extractor
-import logging
 import mysql.connector
-from mysql.connector import errors
 from dotenv import load_dotenv
-from pathlib import Path
-import os
+from kafka import KafkaConsumer, TopicPartition
 
-# Load enviroment from .env_pro
+# Load Environments from .env_pro
 env_path = Path('.') / '.env_pro'
 load_dotenv(dotenv_path=env_path)
 
-
-
 # Insert log to file
-logging.basicConfig(filename=os.getenv("log_file") , filemode='w',
-                   format='%(asctime)s %(name)s %(levelname)s %(message)s',level=os.getenv("log_level"))
-
-
+logging.basicConfig(filename=os.getenv("log_file"), filemode='w',
+                    format='%(asctime)s %(name)s %(levelname)s %(message)s', level=os.getenv("log_level"))
 
 # MySQL Connection
 my_connection = mysql.connector.connect(host=os.getenv("mysql_host"), user=os.getenv("mysql_user"),
-                           passwd=os.getenv("mysql_pass"),
-                           database=os.getenv("mysql_db"))
+                                        passwd=os.getenv("mysql_pass"),
+                                        database=os.getenv("mysql_db"))
 
 
 # List for items updated
@@ -36,13 +31,12 @@ def update_list(data):
     for value in key_list:
         var = eval("update_extractor.%s(data)" % value)
         main_list.append(var)
-    zipped = zip(key_list, main_list)
-    filter_none = list(filter(lambda x: x[1] is not None, zipped))
-    unzipped_a = [x[0] for x in filter_none]
-    unzipped_b = [x[1] for x in filter_none]
-    logging.debug(f"update_list: {unzipped_a} {unzipped_b}")
-    return unzipped_a+unzipped_b
-
+    aggregation_list = zip(key_list, main_list)
+    filter_none = list(filter(lambda x: x[1] is not None, aggregation_list))
+    item_list = [x[0] for x in filter_none]
+    value_list = [x[1] for x in filter_none]
+    logging.debug(f"update_list: {item_list} {value_list}")
+    return item_list + value_list
 
 
 # List for items inserted
@@ -57,70 +51,69 @@ def insert_list(data):
     unzipped_a = [x[0] for x in filter_none]
     unzipped_b = [x[1] for x in filter_none]
     logging.debug(f"insert_list: {unzipped_a} {unzipped_b}")
-    return unzipped_a+unzipped_b
-
+    return unzipped_a + unzipped_b
 
 
 # Update query set
 def update_query(get_list):
-    mean = len(get_list)//2
-    whkey = get_list[0]
-    whval = get_list[mean]
-    table = os.getenv("mysql_table")
-    cnt = 1
-    meancnt = mean+1
+    mean = len(get_list) // 2
+    id_key = get_list[0]
+    id_value = get_list[mean]
+    table_name = os.getenv("mysql_table")
+    counter = 1
+    mean_counter = mean + 1
     query_data = ""
-    while cnt < mean:
-        if isinstance(get_list[meancnt], int):
-            if meancnt == len(get_list)-1:
-                query_data += f"{get_list[cnt]} = {get_list[meancnt]}"
+    while counter < mean:
+        if isinstance(get_list[mean_counter], int):
+            if mean_counter == len(get_list) - 1:
+                query_data += f"{get_list[counter]} = {get_list[mean_counter]}"
             else:
-                query_data += f"{get_list[cnt]} = {get_list[meancnt]}, "
+                query_data += f"{get_list[counter]} = {get_list[mean_counter]}, "
         else:
-            if meancnt == len(get_list)-1:
-                query_data += f"{get_list[cnt]} = '{get_list[meancnt]}'"
+            if mean_counter == len(get_list) - 1:
+                query_data += f"{get_list[counter]} = '{get_list[mean_counter]}'"
             else:
-                query_data += f"{get_list[cnt]} = '{get_list[meancnt]}', "
-        cnt += 1
-        meancnt += 1
-    query = f"update {table} set {query_data} where {whkey} = '{whval}'"
-    return query
+                query_data += f"{get_list[counter]} = '{get_list[mean_counter]}', "
+        counter += 1
+        mean_counter += 1
+    update_query = f"update {table_name} set {query_data} where {id_key} = '{id_value}'"
+    return update_query
 
 
 # Insert query set
 def insert_query(get_list):
-    mean = len(get_list)//2
-    table = os.getenv("mysql_table")
-    cnt = 0
+    mean = len(get_list) // 2
+    table_name = os.getenv("mysql_table")
+    counter = 0
     query_item = ""
     query_value = ""
-    while cnt < mean:
-        meancnt = cnt+mean
-        if isinstance(get_list[meancnt], int):
-            if meancnt == len(get_list)-1:
-                query_item += f"{get_list[cnt]}"
-                query_value += f"{get_list[meancnt]}"
+    while counter < mean:
+        mean_counter = counter + mean
+        if isinstance(get_list[mean_counter], int):
+            if mean_counter == len(get_list) - 1:
+                query_item += f"{get_list[counter]}"
+                query_value += f"{get_list[mean_counter]}"
             else:
-                query_item += f"{get_list[cnt]}, "
-                query_value += f"{get_list[meancnt]}, "
+                query_item += f"{get_list[counter]}, "
+                query_value += f"{get_list[mean_counter]}, "
         else:
-            if meancnt == len(get_list)-1:
-                query_item += f"{get_list[cnt]}"
-                query_value += f"'{get_list[meancnt]}'"
+            if mean_counter == len(get_list) - 1:
+                query_item += f"{get_list[counter]}"
+                query_value += f"'{get_list[mean_counter]}'"
             else:
-                query_item += f"{get_list[cnt]}, "
-                query_value += f"'{get_list[meancnt]}', "
-        cnt += 1
-    query = f"insert into {table}({query_item}) values({query_value})"
-    return query
+                query_item += f"{get_list[counter]}, "
+                query_value += f"'{get_list[mean_counter]}', "
+        counter += 1
+    insert_query = f"insert into {table_name}({query_item}) values({query_value})"
+    return insert_query
 
 
 # Delete query
 def delete_query(get_id):
-    table = os.getenv("mysql_table")
-    mean = len(get_id)//2
-    query = f"delete from `{table}` where `id` = '{get_id[mean]}'"
-    return query
+    table_name = os.getenv("mysql_table")
+    mean = len(get_id) // 2
+    delete_query = f"delete from `{table_name}` where `id` = '{get_id[mean]}'"
+    return delete_query
 
 
 # Execute update query to MySQL
@@ -137,24 +130,24 @@ def update_execute(data):
 
 
 # Execute query to MySQL
-def insert_execute(data,redata):
+def insert_execute(data, redata):
     return my_connection
-    val = data
-    re_val = redata
+    insert_record = data
+    delete_record = redata
     try:
         my_cursor = my_connection.cursor()
-        my_cursor.execute(val)
+        my_cursor.execute(insert_record)
         my_connection.commit()
     except mysql.connector.IntegrityError as e:
         logging.info(f"This query {data} is duplicate")
         my_cursor = my_connection.cursor()
-        my_cursor.execute(re_val)
+        my_cursor.execute(delete_record)
         my_connection.commit()
-        logging.info(f"This query {val} duplicated is delete")
+        logging.info(f"This query {insert_record} duplicated is delete")
         my_cursor = my_connection.cursor()
-        my_cursor.execute(val)
+        my_cursor.execute(insert_record)
         my_connection.commit()
-        logging.info(f"This query {val} deleted is duplicate")
+        logging.info(f"This query {insert_record} deleted is duplicate")
     finally:
         my_cursor.close()
         my_connection.close()
@@ -163,7 +156,7 @@ def insert_execute(data,redata):
 # Consume from Kafka Broker
 consumer = KafkaConsumer(auto_offset_reset=os.getenv("offset_reset_from"),
                          enable_auto_commit=os.getenv("auto_commit"),
-                         group_id=os.getenv("counsumer_group"),
+                         group_id=os.getenv("consumer_group"),
                          bootstrap_servers=[os.getenv("kafka_host")])
 consumer.assign([TopicPartition(os.getenv("kafka_topic"), 0)])
 while True:
@@ -172,7 +165,7 @@ while True:
     json_map = json.loads(msg.value.decode('utf-8'))
     operation_type = json_map['operationType']
     if operation_type == 'insert' or operation_type == 'replace':
-        insert_execute(insert_query(insert_list(json_map)),delete_query(insert_list(json_map)))
+        insert_execute(insert_query(insert_list(json_map)), delete_query(insert_list(json_map)))
         logging.info(f"Offset: {msg.offset} executed")
     elif operation_type == 'update':
         if len(update_list(json_map)) == 2:
